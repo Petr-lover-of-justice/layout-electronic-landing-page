de6f9950cd6b6b9b769c5515aba6f9e2ae595117
Petr <kim.pyoter2017@yandex.ru>
2022-11-23 12:43:28 +0300

    correctionFix

--- js/js.js
+++ js/js.js
@@ -1,58 +1,15 @@
 $(document).ready(function () {
-  let width = $(window).width();
-  if(width<1000){
-    $(".slider").slick({
-      rows: 2,
-      slidesToShow: 2,
-      appendArrows: $(".arrow"),
-    });
-    $(".slider__item")
-    .on("mouseenter", function(){
-      $( this ).children( ".slider__border" ).css({
-          "width": "160px",
-          "margin-left": "0px",
-          "border-bottom": "30px"
-        });
-    })
-    .on("mouseleave", function() {
-      $( this ).children( ".slider__border" ).css({
-          "width": "40px",
-          "margin-left": "176px",
-          "border-bottom": "0px"
-        });
-    })
-  }else{
     $(".slider").slick({
       rows: 2,
       slidesToShow: 4,
       appendArrows: $(".arrow"),
+      responsive: [
+        {
+          breakpoint:1000,
+          settings:{
+            slidesToShow: 2,
+          }
+        }
+      ]
     });
-    $(".slider__item")
-    .on("mouseenter", function(){
-      $( this ).children( ".slider__border" ).css({
-          "width": "288px",
-          "margin-left": "0px",
-          "border-bottom": "30px"
-        });
-    })
-    .on("mouseleave", function() {
-      $( this ).children( ".slider__border" ).css({
-          "width": "40px",
-          "margin-left": "176px",
-          "border-bottom": "0px"
-        });
-    })
-  }
-
-
-  $(".document").on(function () {
-    console.log("dsf");
-      $(".this").slick({
-        rows: 1,
-        slidesToShow: 2,
-        appendArrows: $(".arrow"),
-      });
-      console.log(width);
-  })
-  
 });
